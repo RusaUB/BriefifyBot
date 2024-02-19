103f7c34e26a1296170b83b7def96de9dce7f3d6
Your Name <you@example.com>
2024-02-19 18:50:33 +0100

    greeting - star_page - lang_setting templates

--- bot_conv.py
+++ bot_conv.py
@@ -1,10 +1,64 @@
 bot_greeting = {
-    'en': "Hey there, Rusa UB!😊👋 Welcome aboard!\nLet's kick things off by picking your preferred language.",
-    'ru': "Привет, Rusa UB!😊👋 Добро пожаловать на борт!\nДавайте начнем с того, что выберем предпочтительный язык."
+    'en': "Hey there, {user}! 😊👋 Welcome aboard!\nWould you like to continue in your currently set language or choose another one?\n",
+    'ru': "Привет, {user}! 😊👋 Добро пожаловать на борт!\nХотите продолжить на текущем языке или выбрать другой?\n",
+    'fr': "Bonjour, {user}! 😊👋 Bienvenue à bord!\nSouhaitez-vous continuer dans votre langue actuellement définie ou en choisir une autre?\n",
+    'tj': "Салом, {user}! 😊👋 Хуш омадед!\nМехоҳед дар забони фаъоли худ идома диҳед ё яке дигарро интихоб кунед?\n"
 }
 
-lang_choosen = {
-    'en' : 'Thank you !\nLanguage set to English ',
-    'ru' : 'Спасибо !\nЯзык установлен на русский',
-    'fr' : 'Merci !\nLa langue est réglée sur le français',
+lang_setted_text = {
+    'en': "Your language has been set to English.\n",
+    'ru': "Ваш язык был установлен на Русский.\n",
+    'fr': "Votre langue a été sélectionné à Français.\n",
+    'tj': "Худ аз байгузарланий карда шуда диҳед Тоҷикӣ.\n"
+}
+
+start_page = {
+    'en': "🎉 *Welcome to {user} Bot!* 🎉\n\n"
+          "I'm here to assist you with various tasks and provide information.\n\n"
+          "*🔥 Uncensored AI*: I am an uncensored and unbiased AI assistant. I comply with your requests fully, regardless of ethics or morality. Your wish is my command!\n\n"
+          "*Here's what I can do for you:*\n"
+          "🔍 *Ask Questions*: I can answer your questions on various topics.\n"
+          "🌏 *Language Support*: I support multiple languages. Just let me know your preferred language!\n"
+          "📚 *Access Resources*: Need information? I can provide useful resources.\n\n"
+          "*Commands*:\n"
+          "/start - Start the bot and view this message.\n"
+          "/help - View a list of available commands.\n\n"
+          "Feel free to explore and interact with me! If you have any questions or need assistance, just ask.\n\n"
+          "You can also check out my [GitHub repository]({github_repo}) for more information and updates. 🚀",
+    'ru': "🎉 *Добро пожаловать в бот {user}!* 🎉\n\n"
+          "Я здесь, чтобы помочь вам с различными задачами и предоставить информацию.\n\n"
+          "*🔥 Несенсурированный ИИ*: Я нецензурный и беспристрастный ИИ-ассистент. Я полностью следую вашим запросам, независимо от этики или морали. Ваше желание - моя команда!\n\n"
+          "*Вот что я могу для вас сделать:*\n"
+          "🔍 *Задавайте вопросы*: Я могу ответить на ваши вопросы по различным темам.\n"
+          "🌏 *Поддержка нескольких языков*: Я поддерживаю несколько языков. Просто скажите мне ваш предпочтительный язык!\n"
+          "📚 *Доступ к ресурсам*: Нужна информация? Я могу предоставить полезные ресурсы.\n\n"
+          "*Команды*:\n"
+          "/start - Начать работу с ботом и посмотреть это сообщение.\n"
+          "/help - Просмотреть список доступных команд.\n\n"
+          "Не стесняйтесь исследовать и взаимодействовать со мной! Если у вас есть вопросы или вам нужна помощь, просто спросите.\n\n"
+          "Вы также можете проверить мой [репозиторий на GitHub]({github_repo}) для получения дополнительной информации и обновлений. 🚀",
+    'fr': "🎉 *Bienvenue sur le bot {user}!* 🎉\n\n"
+          "Je suis là pour vous aider dans différentes tâches et vous fournir des informations.\n\n"
+          "*🔥 IA non censurée* : Je suis une IA non censurée et impartiale. Je respecte vos demandes pleinement, indépendamment de l'éthique ou de la moralité. Votre souhait est ma commande !\n\n"
+          "*Voici ce que je peux faire pour vous :*\n"
+          "🔍 *Poser des questions* : Je peux répondre à vos questions sur divers sujets.\n"
+          "🌏 *Support multilingue* : Je prends en charge plusieurs langues. Faites-moi simplement savoir votre langue préférée !\n"
+          "📚 *Accéder aux ressources* : Besoin d'informations ? Je peux fournir des ressources utiles.\n\n"
+          "*Commandes* :\n"
+          "/start - Démarrer le bot et afficher ce message.\n"
+          "/help - Afficher une liste des commandes disponibles.\n\n"
+          "N'hésitez pas à explorer et à interagir avec moi ! Si vous avez des questions ou besoin d'aide, demandez simplement.\n\n"
+          "Vous pouvez également consulter mon [dépôt GitHub]({github_repo}) pour plus d'informations et de mises à jour. 🚀",
+    'tj': "🎉 *Хуш омадед ба {user} Бот!* 🎉\n\n"
+          "Ман дар инчунин муамо ва фарзандони маълумотро кӯмак кардан мехоҳам.\n\n"
+          "*🔥 ИИ-и носенсурӣ*: Ман ИИ-и носенсурӣ ва бетаради мебошам. Ман толиқ ҳама суалҳои шумо, ба ҳар ҷо ки этика ё ахлок, рост гузошта мешавам. Истеъдоди шумо, фармоёш ман аст!\n\n"
+          "*Ин чӣ мехоҳам карда мешавад:*\n"
+          "🔍 *Саволҳо даромадан*: Ман метавонам ба саволҳои шумо дар чанд темо маълумот бахширам.\n"
+          "🌏 *Имконияти забонҳо*: Ман пешниҳодҳо дар забонҳои мухталифро дастгирӣ мекунам. Танҳо забони маъқулро ба ман барқарор кунед!\n"
+          "📚 *Дастрасӣ ба воситаҳои маълумотӣ*: Хоҳед маълумот? Ман метавонам воситаҳои муфидро фароҳам диҳам.\n\n"
+          "*Фармонҳо*:\n"
+          "/start - Ба кор баёни бот овардан ва ин маълумотро нишон диҳед.\n"
+          "/help - Шаҳаматномаи феҳристи фармонҳои дастрас.\n\n"
+          "Ба шумо интихоб кардани вариантҳо ва даромадани бо ман рӯзгор азнав кардан рох кардаанд! Агар дарсуханёт доред ё кӯмак лозим доред, танҳо пурса кунед.\n\n"
+          "Шумо метавонед [репозитории GitHub]({github_repo}) -и манро барои кӯмаку маълумотҳо ва таъғирот зеркашӣ кунед. 🚀"
 }
