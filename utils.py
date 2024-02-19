98d639b07ba7526e5c6893bad9d2495d0bc78430
Your Name <you@example.com>
2024-02-19 18:51:16 +0100

    context formating

--- utils.py
+++ utils.py
@@ -1,2 +1,5 @@
-def message_text(language_code : str, message : dict) -> str:
+def message_text(language_code: str, message: dict, context: dict = None) -> str:
+    if context is not None:
+        formatted_message = message[language_code].format(**context)
+        return formatted_message
     return message[language_code]
